bf2412b8d3364f59af652f1f9d257ecf69e67269
tasniazannat65 <tasniazannat9@gmail.com>
2025-09-20 17:53:38 +0600

    added categories

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,16 +1,37 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Category from './Category';
 
 const Categories = ({foodPromise}) => {
+    const [showBuyFood, setShowBuyFood] = useState([]);
+    const handleShowBuyFood = (category)=>{
+        setShowBuyFood([...showBuyFood, category])
+    }
     const categoriesData = use(foodPromise)
      const categories = categoriesData.categories;
     
     return (
         <div>
-            <h1>All foods item</h1>
+            <h1 className='text-2xl font-bold'>All foods item {categories.length}</h1>
+            <h3 className='text-xl font-semibold'>Show Buy food item:{showBuyFood.length} </h3>
+            <ol>
+                 {
+                    showBuyFood.map(category=> <li key={category.idCategory} className='text-xl font-semibold'>{category.strCategory}</li>
+                        
+                    )
+                }
+            </ol>
+            <div className='flex'>
+                
+                {
+                    showBuyFood.map((category, index)=> <img className='w-32' key={index} src={category.strCategoryThumb}></img>)
+                }
+            </div>
             <div className='grid grid-cols-3 gap-4 w-7xl mx-auto my-10'>
                 {
-                    categories.map(category=> <Category key={category.idCategory} category={category}></Category>)
+                    categories.map(category=> <Category key={category.idCategory} category={category}
+                    handleShowBuyFood={handleShowBuyFood}
+                    
+                    ></Category>)
                 }
             </div>
         </div>
--- src/Category.jsx
+++ src/Category.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Category = ({category}) => {
+const Category = ({category, handleShowBuyFood}) => {
+    const [buyFood, setBuyFood] = useState(false);
+    const handleBuyFood = () => {
+        setBuyFood(!buyFood)
+        handleShowBuyFood(category)
+    }
     
     return (
-        <div className=' p-4 rounded-xl shadow-sm space-y-4'>
+        <div className={`p-4 rounded-xl shadow-sm space-y-4 ${buyFood?'bg-purple-300':'bg-white'}`}>
            <img className='w-full mx-auto rounded-lg' src={category.strCategoryThumb} alt="" />
            <h3 className='text-xl font-bold'>{category.strCategory}</h3>
            <p className='font-medium text-lg'>{category.strCategoryDescription}</p>
+           <button className='p-4 bg-yellow-500 rounded-lg cursor-pointer text-white font-bold ml-4' onClick={handleBuyFood}>
+            {
+                buyFood? 'Food Ordered':'Food not Ordered'
+            }
+           </button>
             
         </div>
     );
